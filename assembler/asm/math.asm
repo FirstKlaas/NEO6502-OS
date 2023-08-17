280d5b2e4d1487cd279246a3ef49263c1792bc92
FirstKlaas <klaas.nebuhr@gmail.com>
2023-08-17 07:50:04 +0200

    Added a (untested) random function in math

--- assembler/asm/math.asm
+++ assembler/asm/math.asm
@@ -40,3 +40,39 @@ htd1:
     cld 
     rts
 }
+
+// AX+ Tinyrand8
+// A fast 8-bit random generator with an internal 16bit state
+//
+// Algorithm, implementation and evaluation by Wil
+// This version stores the seed as arguments and uses self-modifying code
+// The name AX+ comes from the ASL, XOR and addition operation
+//
+// Size: 15 Bytes (not counting the set_seed function)
+// Execution time: 18 (without RTS)
+// Period 59748
+// Source: https://codebase64.org/doku.php?id=base:ax_tinyrand8
+
+rand8:	
+	lda rand8b1:#31
+	asl
+	eor rand8a1:#53
+	sta rand8b1
+	adc rand8a1
+	sta rand8a1
+	rts
+
+// sets the seed based on the value in A
+// always sets a1 and b1 so that a cycle with maximum period is chosen
+// constants 217 and 21263 have been derived by simulation
+set_seed:
+	pha
+	and #217
+	clc
+	adc #<21263
+	sta rand8a1
+	pla
+	and #255-217
+	adc #>21263
+	sta rand8b1
+	rts
